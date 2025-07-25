3cb1558cb52d3234112b74a37f7566d26817f351
sadeghbiglar <sbmail555@gmail.com>
2025-07-25 12:06:58 +0330

    run Authentication step features of mary bootcamp

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,28 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+ 
+// Public routes
+Volt::route('/register', 'register'); 
 
-// Volt::route('/', 'users.index');
-Volt::route('/', 'index');                          // Home 
-Volt::route('/users', 'users.index');               // User (list) 
-Volt::route('/users/create', 'users.create');       // User (create) 
-Volt::route('/users/{user}/edit', 'users.edit');    // User (edit) 
+// Users will be redirected to this route if not logged in
+Volt::route('/login', 'login')->name('login');
+ 
+// Define the logout
+Route::get('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+ 
+    return redirect('/');
+});
+ 
+// Protected routes here
+Route::middleware('auth')->group(function () {
+    Volt::route('/', 'index');
+    Volt::route('/users', 'users.index');
+    Volt::route('/users/create', 'users.create');
+    Volt::route('/users/{user}/edit', 'users.edit');
+    // ... more
+});
