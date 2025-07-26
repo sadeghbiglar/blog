72f5d4bbcd3d2c693c5de1acd7ef35d0ba80e054
sadeghbiglar <sbmail555@gmail.com>
2025-07-27 01:14:53 +0330

    test github actions 4

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -86,7 +86,7 @@ public function users(): LengthAwarePaginator
 
 <div>
     <!-- HEADER -->
-    <x-header title="Hello" separator progress-indicator>
+    <x-header title="لیست کاربران" separator progress-indicator>
         <x-slot:middle class="!justify-end">
             <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
         </x-slot:middle>
